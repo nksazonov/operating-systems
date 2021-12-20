c64c320a0d1158856df43fb13fd03b58e8e240fa
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-12-20 19:45:30 +0200

    [init] lab3

---
+++ lab3/src/lab3/Instruction.java
@@ -0,0 +1,14 @@
+package lab3;
+
+public class Instruction
+{
+  public String inst;
+  public long addr;
+
+  public Instruction( String inst, long addr ) 
+  {
+    this.inst = inst;
+    this.addr = addr;
+  } 	
+
+}
